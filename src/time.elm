05b7b5b4eaac27a9ba3d7915886119c8ed814f0a
atrophyworks <atrophyworks@gmail.com>
2019-03-21 23:22:15 +0900

    add time elm

---
+++ src/time.elm
@@ -0,0 +1,103 @@
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Task
+import Time
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { zone : Time.Zone
+    , time : Time.Posix
+    , pause : Bool
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model Time.utc (Time.millisToPosix 0) True
+    , Task.perform AdjustTimeZone Time.here
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Tick Time.Posix
+    | AdjustTimeZone Time.Zone
+    | Pause
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick newTime ->
+            ( { model | time = newTime }
+            , Cmd.none
+            )
+
+        AdjustTimeZone newZone ->
+            ( { model | zone = newZone }
+            , Cmd.none
+            )
+
+        Pause ->
+            ( { model | pause = not model.pause }
+            , Cmd.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    case model.pause of
+        True ->
+            Time.every 1000 Tick
+
+        _ ->
+            Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    let
+        hour =
+            String.fromInt (Time.toHour model.zone model.time)
+
+        minute =
+            String.fromInt (Time.toMinute model.zone model.time)
+
+        second =
+            String.fromInt (Time.toSecond model.zone model.time)
+    in
+    div []
+        [ h1 [] [ Html.text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+        , button [ onClick Pause ] [ Html.text "pause" ]
+        ]
